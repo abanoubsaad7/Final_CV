18e1c38f6ec8d45884fb765edccb52e50a65e246
Abanoub Saad <88698601+abanoubsaad7@users.noreply.github.com>
2023-06-21 22:58:22 +0300

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $("#gmail").click(function() {
+    $("form.gmail").slideToggle();
+    })
+  $("button.whats").click(function(){
+    $("h2.whats").fadeToggle(2250);
+  });
+});
+
