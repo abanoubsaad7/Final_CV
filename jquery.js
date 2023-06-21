b9857839ac0c5882066fd94c8686037ce61cbf09
Abanoub Saad <88698601+abanoubsaad7@users.noreply.github.com>
2023-06-22 00:33:37 +0300

    ..

--- jquery.js
+++ jquery.js
@@ -2,8 +2,5 @@ $(document).ready(function(){
   $("#gmail").click(function() {
     $("form.gmail").slideToggle();
     })
-  $("button.whats").click(function(){
-    $("h2.whats").fadeToggle(2250);
-  });
 });
 
